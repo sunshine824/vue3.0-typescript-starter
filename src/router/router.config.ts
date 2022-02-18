8eae3c135498509293a83e6caa56c0fca577d25d
sunshine824 <cx19950824>
2022-02-18 16:31:26 +0800

    添加404兜底路由

--- src/router/router.config.ts
+++ src/router/router.config.ts
@@ -2,41 +2,43 @@ import { RouteRecordRaw } from 'vue-router'
 import { LevelBasicLayout, RouteLayout } from '@/layouts'
 
 // 导航路由
-const Routes: Array<RouteRecordRaw> = []
+const Routes: Array<RouteRecordRaw> = [
+  {
+    path: '/403',
+    name: '403',
+    component: () =>
+      import(/* webpackChunkName: "403" */ '@/views/exception/403'),
+    meta: { title: '403', permission: ['exception'], hidden: true },
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: () =>
+      import(/* webpackChunkName: "404" */ '@/views/exception/404'),
+    meta: { title: '404', permission: ['exception'], hidden: true },
+  },
+  {
+    path: '/500',
+    name: '500',
+    component: () =>
+      import(/* webpackChunkName: "500" */ '@/views/exception/500'),
+    meta: { title: '500', permission: ['exception'], hidden: true },
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'error',
+    component: () =>
+      import(/* webpackChunkName: "500" */ '@/views/exception/404'),
+    meta: { title: '404', hidden: true },
+  },
+]
 
 // 主路由
 const mainRoutes: RouteRecordRaw = {
   path: '/',
   redirect: '/login',
   component: LevelBasicLayout,
-  children: [
-    // ...Routes,
-    {
-      path: '/403',
-      name: '403',
-      component: () =>
-        import(/* webpackChunkName: "403" */ '@/views/exception/403'),
-      meta: { title: '403', permission: ['exception'], hidden: true },
-    },
-    {
-      path: '/404',
-      name: '404',
-      component: () =>
-        import(/* webpackChunkName: "404" */ '@/views/exception/404'),
-      meta: { title: '404', permission: ['exception'], hidden: true },
-    },
-    {
-      path: '/500',
-      name: '500',
-      component: () =>
-        import(/* webpackChunkName: "500" */ '@/views/exception/500'),
-      meta: { title: '500', permission: ['exception'], hidden: true },
-    },
-    // {
-    //   path: '/:pathMatch(.*)*',
-    //   redirect: { name: '404' },
-    // },
-  ],
+  children: [],
 }
 
 // 基础路由
@@ -49,4 +51,4 @@ const baseRoutes: Array<RouteRecordRaw> = [
   },
 ]
 
-export { mainRoutes, baseRoutes }
+export { mainRoutes, baseRoutes, Routes }
