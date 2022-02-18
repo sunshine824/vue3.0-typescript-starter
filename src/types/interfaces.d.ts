6a0caee4261df3629544b01737ade67a9e91ea2b
sunshine824 <cx19950824>
2022-02-18 09:56:36 +0800

    添加工具库

--- src/types/interfaces.d.ts
+++ src/types/interfaces.d.ts
@@ -8,47 +8,87 @@
 declare namespace StoreState {
   // 接口返回结构
   export interface Result {
-    code: number;
-    data: any;
-    status: boolean;
-    msg: string;
+    code: number
+    data: any
+    status: boolean
+    msg: string
   }
 
   // 请求参数结构
   export interface FetchParams {
-    url: string;
-    data: object | string;
-    [propName: string]: any;
+    url: string
+    data: object | string
+    [propName: string]: any
   }
 
   // 权限返回实体
   export interface Role {
-    icon?: string;
-    list?: Role[];
-    menuId?: number;
-    name: string;
-    perms?: string;
-    requestPath?: string;
-    url: string;
-    [propName: string]: any;
+    icon?: string
+    list?: Role[]
+    menuId?: number
+    name: string
+    perms?: string
+    requestPath?: string
+    url: string
+    [propName: string]: any
   }
 
   // layout布局
   export interface Layout {
     labelCol: {
-      span: number;
-    };
+      span: number
+    }
     wrapperCol: {
-      span: number;
-    };
+      span: number
+    }
   }
 
   // 分页参数
   export interface Pagination {
-    current?: number;
-    position?: string;
-    pageSize?: number;
-    total?: number;
-    showTotal?: (total: number, range: [number, number]) => any;
+    current?: number
+    position?: string
+    pageSize?: number
+    total?: number
+    showQuickJumper?: boolean
+    showTotal?: (total: number, range: [number, number]) => any
+  }
+
+  // 上传文件
+  export interface FileItem {
+    uid: string
+    name?: string
+    status?: string
+    response?: string
+    url?: string
+    type?: string
+    size: number
+    originFileObj: any
+  }
+
+  export interface FileInfo {
+    file: FileItem
+    fileList: FileItem[]
+  }
+
+  // 资源类型
+  export interface ResourceTypes {
+    '1': string
+    '2': string
+    '3': string
+    [key: string]: string
+  }
+
+  // 列表mixin
+  export interface TableMixinOptions {
+    queryTableApi: ({}) => any
+    deleteApi: ({}) => any
+  }
+
+  // 列表查询参数
+  export interface TableQueryOptions {
+    orgId?: string
+    pageNo: number
+    pageSize: number
+    [propName: string]: any
   }
 }
---
+++ src/utils/const.ts
@@ -0,0 +1,24 @@
+// 定义分页类
+export class Pagination {
+  private Page: StoreState.Pagination = {
+    position: 'bottom',
+    showQuickJumper: true,
+    current: 1,
+    pageSize: 10,
+    total: 0,
+    showTotal: (total: number) => `总 ${total} 条`,
+  }
+
+  constructor(params?: StoreState.Pagination) {
+    Object.assign(this.Page, params)
+  }
+
+  init() {
+    return this.Page
+  }
+}
+
+// modal公共属性
+export const modalProps = {
+  maskClosable: false,
+}
