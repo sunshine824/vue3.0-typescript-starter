9a43c3b55d3dea774b12c04e2c7a5c13dd51305c
sunshine824 <cx19950824>
2022-02-23 09:17:45 +0800

    优化api

--- src/api/user.ts
+++ src/api/user.ts
@@ -4,26 +4,29 @@ import { IUserApi } from './types/user'
 
 const UserApi: IUserApi = {
   // 登录
-  login: (params) =>
-    fetch({
+  login: (params) => {
+    return fetch({
       method: 'post',
       url: '/login',
       data: params,
-    }),
+    })
+  },
   // 注销登录
-  logout: (params) =>
-    fetch({
+  logout: (params) => {
+    return fetch({
       method: 'get',
       url: '/logout',
       data: params,
-    }),
+    })
+  },
   // 获取权限列表
-  getPermissionsList: (params) =>
-    fetch({
+  getPermissionsList: (params) => {
+    return fetch({
       method: 'get',
       url: '/navPerson',
       data: qs.stringify(params),
-    }),
+    })
+  },
 }
 
 export default UserApi
