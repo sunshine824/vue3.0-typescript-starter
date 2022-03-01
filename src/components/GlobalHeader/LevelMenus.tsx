dc968f01b7f62764ae39f774b4807655878ad915
sunshine824 <cx19950824>
2022-03-01 09:46:12 +0800

    优化代码

--- src/components/GlobalHeader/LevelMenus.tsx
+++ src/components/GlobalHeader/LevelMenus.tsx
@@ -5,58 +5,55 @@ import router from '../../router'
 import styles from './index.module.less'
 
 const LevelMenus = defineComponent({
-  name: 'LevelMenus',
-  props: {
-    menuLists: {
-      type: Array,
-      default: [],
-    },
-  },
-  setup(props, { slots }) {
-    const route = useRoute() // 路由实例
+	name: 'LevelMenus',
+	props: {
+		menuLists: {
+			type: Array,
+			default: []
+		}
+	},
+	setup(props, { slots }) {
+		const route = useRoute() // 路由实例
 
-    const router = useRouter()
+		const router = useRouter()
 
-    const baseUrl = ref() // 当前跳转基础路由
+		const baseUrl = ref() // 当前跳转基础路由
 
-    onMounted(() => {
-      const paths = route.path.split('/')
-      if (paths.length > 1) {
-        baseUrl.value = '/' + paths[1]
-      }
-    })
+		onMounted(() => {
+			const paths = route.path.split('/')
+			if (paths.length > 1) {
+				baseUrl.value = '/' + paths[1]
+			}
+		})
 
-    // 路由跳转
-    const skipTo = (item: RouteRecordRaw) => {
-      router.push(item.path)
-    }
+		// 路由跳转
+		const skipTo = (item: RouteRecordRaw) => {
+			router.push(item.path)
+		}
 
-    // 监听路由变化
-    watch(route, (val) => {
-      const paths = val.path.split('/')
-      if (paths.length > 1) {
-        baseUrl.value = '/' + paths[1]
-      }
-    })
+		// 监听路由变化
+		watch(route, val => {
+			const paths = val.path.split('/')
+			if (paths.length > 1) {
+				baseUrl.value = '/' + paths[1]
+			}
+		})
 
-    return () => (
-      <div class={styles['level-menus']}>
-        <ul class={styles['level-menus-list']}>
-          {(props.menuLists as RouteRecordRaw[]).map((menu) => {
-            return (
-              <li
-                onClick={skipTo.bind(this, menu)}
-                class={menu.path == baseUrl.value && styles['active']}
-              >
-                <span class={`iconfont ${menu?.meta?.icon} menu-icon`}></span>
-                <span class={styles['title']}>{menu?.meta?.title}</span>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-    )
-  },
+		return () => (
+			<div class={styles['level-menus']}>
+				<ul class={styles['level-menus-list']}>
+					{(props.menuLists as RouteRecordRaw[]).map(menu => {
+						return (
+							<li onClick={skipTo.bind(this, menu)} class={menu.path == baseUrl.value && styles['active']}>
+								<span class={`iconfont ${menu?.meta?.icon} menu-icon`}></span>
+								<span class={styles['title']}>{menu?.meta?.title}</span>
+							</li>
+						)
+					})}
+				</ul>
+			</div>
+		)
+	}
 })
 
 export default LevelMenus
--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -8,110 +8,110 @@ const BaseUrl = import.meta.env.VITE_API_BASE_URL as string
 
 // create an axios instance
 const service: AxiosInstance = axios.create({
-  baseURL: BaseUrl, // 正式环境
-  timeout: 60 * 1000,
-  headers: {},
+	baseURL: BaseUrl, // 正式环境
+	timeout: 60 * 1000,
+	headers: {}
 })
 
 /**
  * 请求拦截
  */
 service.interceptors.request.use(
-  (config: AxiosRequestConfig) => {
-    config.headers.common.Authorization = getToken() // 请求头带上token
-    config.headers.common.token = getToken()
-    return config
-  },
-  (error) => Promise.reject(error),
+	(config: AxiosRequestConfig) => {
+		config.headers.common.Authorization = getToken() // 请求头带上token
+		config.headers.common.token = getToken()
+		return config
+	},
+	error => Promise.reject(error)
 )
 
 /**
  * 响应拦截
  */
 service.interceptors.response.use(
-  (response: AxiosResponse) => {
-    if (response.status == 201 || response.status == 200) {
-      const { code, status, msg } = response.data
-      if (code == 401) {
-        Modal.warning({
-          title: 'token出错',
-          content: 'token失效，请重新登录！',
-          onOk: () => {
-            sessionStorage.clear()
-          },
-        })
-      } else if (code == 200) {
-        if (status) {
-          // 接口请求成功
-          msg && Message.success(msg) // 后台如果返回了msg，则将msg提示出来
-          return Promise.resolve(response) // 返回成功数据
-        }
-        // 接口异常
-        msg && Message.warning(msg) // 后台如果返回了msg，则将msg提示出来
-        return Promise.reject(response) // 返回异常数据
-      } else {
-        // 接口异常
-        msg && Message.error(msg)
-        return Promise.reject(response)
-      }
-    }
-    return response
-  },
-  (error) => {
-    if (error.response.status) {
-      switch (error.response.status) {
-        case 500:
-          Notification.error({
-            message: '温馨提示',
-            description: '服务异常，请重启服务器！',
-          })
-          break
-        case 401:
-          Notification.error({
-            message: '温馨提示',
-            description: '服务异常，请重启服务器！',
-          })
-          break
-        case 403:
-          Notification.error({
-            message: '温馨提示',
-            description: '服务异常，请重启服务器！',
-          })
-          break
-        // 404请求不存在
-        case 404:
-          Notification.error({
-            message: '温馨提示',
-            description: '服务异常，请重启服务器！',
-          })
-          break
-        default:
-          Notification.error({
-            message: '温馨提示',
-            description: '服务异常，请重启服务器！',
-          })
-      }
-    }
-    return Promise.reject(error.response)
-  },
+	(response: AxiosResponse) => {
+		if (response.status == 201 || response.status == 200) {
+			const { code, status, msg } = response.data
+			if (code == 401) {
+				Modal.warning({
+					title: 'token出错',
+					content: 'token失效，请重新登录！',
+					onOk: () => {
+						sessionStorage.clear()
+					}
+				})
+			} else if (code == 200) {
+				if (status) {
+					// 接口请求成功
+					msg && Message.success(msg) // 后台如果返回了msg，则将msg提示出来
+					return Promise.resolve(response) // 返回成功数据
+				}
+				// 接口异常
+				msg && Message.warning(msg) // 后台如果返回了msg，则将msg提示出来
+				return Promise.reject(response) // 返回异常数据
+			} else {
+				// 接口异常
+				msg && Message.error(msg)
+				return Promise.reject(response)
+			}
+		}
+		return response
+	},
+	error => {
+		if (error.response.status) {
+			switch (error.response.status) {
+				case 500:
+					Notification.error({
+						message: '温馨提示',
+						description: '服务异常，请重启服务器！'
+					})
+					break
+				case 401:
+					Notification.error({
+						message: '温馨提示',
+						description: '服务异常，请重启服务器！'
+					})
+					break
+				case 403:
+					Notification.error({
+						message: '温馨提示',
+						description: '服务异常，请重启服务器！'
+					})
+					break
+				// 404请求不存在
+				case 404:
+					Notification.error({
+						message: '温馨提示',
+						description: '服务异常，请重启服务器！'
+					})
+					break
+				default:
+					Notification.error({
+						message: '温馨提示',
+						description: '服务异常，请重启服务器！'
+					})
+			}
+		}
+		return Promise.reject(error.response)
+	}
 )
 
 interface Http {
-  fetch<T>(params: AxiosRequestConfig): Promise<StoreState.ResType<T>>
+	fetch<T>(params: AxiosRequestConfig): Promise<StoreState.ResType<T>>
 }
 
 const http: Http = {
-  fetch(params) {
-    return new Promise((resolve, reject) => {
-      service(params)
-        .then((res) => {
-          resolve(res.data)
-        })
-        .catch((err) => {
-          reject(err.data)
-        })
-    })
-  },
+	fetch(params) {
+		return new Promise((resolve, reject) => {
+			service(params)
+				.then(res => {
+					resolve(res.data)
+				})
+				.catch(err => {
+					reject(err.data)
+				})
+		})
+	}
 }
 
 export default http['fetch']
